1274162b28784efa61c3f31821be8dfb82d67bcb
Jeremy Brunel <bj901398@gmail.com>
2020-08-10 13:57:27 +0200

    navbar color fct anonymousUSer

--- src/main/java/com/astek/myquotes/controller/MainController.java
+++ src/main/java/com/astek/myquotes/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {	
 
-	@GetMapping({ "", "/", "/index" })
+	@GetMapping({ "", "/", "/index" , "/css/**", "/img/**" })
 	public String index() {
 		return "index";
 	}
--- src/main/java/com/astek/myquotes/security/SecurityConfig.java
+++ src/main/java/com/astek/myquotes/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.astek.myquotes.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,8 +10,6 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -56,5 +53,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
+	
 }
